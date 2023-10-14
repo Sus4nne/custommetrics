aea920da6c2db672789d3bd717576ab3f8cf64b9
Sus4nne <victorisu@gmail.com>
2023-10-14 15:38:06 +0200

    added MeterRegistery and counter

--- src/main/java/nl/pieterse/custommetrics/PastryService.java
+++ src/main/java/nl/pieterse/custommetrics/PastryService.java
@@ -1,11 +1,23 @@
 package nl.pieterse.custommetrics;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PastryService {
 
+    CompositeMeterRegistry meterRegistry;
+
+    private Counter pastryCounter;
+
+    public PastryService(CompositeMeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+        pastryCounter = meterRegistry.counter("order.pastry");
+    }
+
     public String orderPastry(int amount) {
+        pastryCounter.increment(amount);
         return String.format("Thank you for ordering %s pastries!", amount);
     }
 
