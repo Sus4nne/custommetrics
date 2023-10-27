526ad77fda1f1d02f8ca8c37952f8aaacd0e3f6b
Sus4nne <victorisu@gmail.com>
2023-10-27 13:57:15 +0200

    added to readme, grafana dashboard json, changes in demo script

--- README.md
+++ README.md
@@ -12,4 +12,7 @@ and http://localhost:8080/actuator/metrics/pastry.amount.orders
 
 ### Use the metrics in Grafana
 #### Create a Prometheus data source
+In the docker compose stack Prometheus runs at http://prometheus:9090
+
 #### Create a Grafana dashboard
+You can add panels for the metrics from scratch. You can also import the provided dashboard. It is the json file in this repo named `Pastry orders-1698403567304.json`.
--- demoscript.py
+++ demoscript.py
@@ -10,11 +10,11 @@ while time.time() - start_time < 120:
     amount = random.randint(2, 12)
     total_amount += amount
     total_orders += 1
-    print('Order {} with {} pastries, total = {}'.format(total_orders, amount, total_amount))
+    print('>> Order {} with {} pastries, total = {}'.format(total_orders, amount, total_amount))
     response = requests.post('http://localhost:8080/pastries', data={'amount': amount})
     print('Response from pastry shop: {}'.format(response.text))
     time.sleep(25)
 
 print('Done')
-print('Total amount: {}'.format(total_amount))
+print('Total pastries ordered: {}'.format(total_amount))
 print('Total orders: {}'.format(total_orders))
